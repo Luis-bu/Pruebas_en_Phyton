52e8b56c4fb96d12f80a9376120ff6dae3318185
luis-bu <luisbuemaxx@gmail.com>
2024-10-08 10:19:52 -0500

    cositas

---
+++ estudio/condcionales.py
@@ -0,0 +1,8 @@
+print("Ingresa tu edad master")
+edad=int(input(""))
+if edad>=18:
+    print("Si puedes entrar a la fiesta")
+
+else:
+    print("No entras a la fiesta")    
+    
--- estudio/variables.py
+++ estudio/variables.py
@@ -5,7 +5,7 @@ saludo= "Hola " + nombre + " ¿Como estas?"
 #borramos la variable
 del nombre
 
-#F STRING QUE PASA TODO TIPO DE DATOS A TEXTO
+#F STRING QUE PASA todo TIPO DE DATOS A TEXTO
 personaQueNoCreoQueNadieSeLlameAsi=False
 saludo2= f"Hola {personaQueNoCreoQueNadieSeLlameAsi} como estas?"
 
@@ -13,10 +13,11 @@ saludo2= f"Hola {personaQueNoCreoQueNadieSeLlameAsi} como estas?"
 print(saludo)
 print(saludo2)
 
+#Operadores de pertenencia (in / not in)
 #Vemos si el String "Pedro" esta en mi variable saludo (booleano)
 print("Pedro" in saludo)        #True
 
 #"Mario" no esta en mi variable bienvenida? (booleano)
-print("False" not in saludo2)    #False
+print("Mario" not in saludo2)    #False
 
-#Operadores de pertenencia (in / not in)
+
